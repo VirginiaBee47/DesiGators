6178b86e1865fed077d1348e86259dd25a274c61
ben-covert2020 <66711984+ben-covert2020@users.noreply.github.com>
2023-11-13 18:29:23 -0500

    Update gui.py

--- src/gui.py
+++ src/gui.py
@@ -55,6 +55,7 @@ class MassUpdater(QRunnable):
     def run(self):
         print("Thread started.")
         try:
+            print(self.control)
             while self.control['measure_mass']:
                     readings = self._array.take_measurement()
                     self.signals.result.emit(readings)
