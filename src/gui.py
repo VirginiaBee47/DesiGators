5d5b94bcdeb57180b175bf24dc4a61b01a911eb1
ben-covert2020 <66711984+ben-covert2020@users.noreply.github.com>
2024-02-29 12:12:26 -0500

    Update gui.py

--- src/gui.py
+++ src/gui.py
@@ -513,7 +513,7 @@ class ChamberTabWidget(QWidget):
         conditions_1 = QLabel('Operating Conditions 1')
         conditions_box_layout.addWidget(conditions_1)
         separator = QFrame()
-        separator.setFrameShape(QFrame.VLine)
+        separator.setFrameShape(4)
         separator.setLineWidth(2)
         conditions_box_layout.addWidget(separator)
         conditions_2 = QLabel('Operating Conditions 2')
